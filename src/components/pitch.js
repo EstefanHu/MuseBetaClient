5b19e2c193f982c07a856db8a633acfc666ac8b6
Estefan Hu <estefanhu074@gmail.com>
2020-06-07 14:42:40 -0700

    added pitch component

---
+++ src/components/pitch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: '300px',
+    padding: '20px 30px',
+    borderTop: '1px solid lightgrey'
+  }
+}
+
+export const Pitch = ({ title, description }) => {
+  return (
+    <article style={styles.container}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </article>
+  )
+}
