364c90c54cb425eef424b3fb3998407acf4c7dc6
Estefan Hu <estefanhu074@gmail.com>
2020-06-14 23:00:45 -0700

    added last of privacy info before section 3

--- src/views/Privacy.js
+++ src/views/Privacy.js
@@ -355,11 +355,55 @@ export const Privacy = () => {
             </ul>
           </article>
         </section>
-        
-        <section id='track'></section>
-        <section id='cali'></section>
-        <section id='update'></section>
-        <section id='contact'></section>
+
+        <section style={styles.section} id='track'>
+          <h3 style={styles.title}>11. CONTROLS FOR DO-NOT-TRACK FEATURES</h3>
+          <article style={styles.article}>
+            <p style={styles.paragraph}>
+              Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track (“DNT”) feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. No uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy policy.
+            </p>
+          </article>
+        </section>
+
+        <section style={styles.section} id='cali'>
+          <h3 style={styles.title}>12. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</h3>
+          <article style={styles.article}>
+            <p style={styles.paragraph}>
+              <em><strong>In short:</strong> Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.</em>
+            </p>
+            <p style={styles.paragraph}>
+              California Civil Code Section 1798.83, also known as the “Shine The Light” law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.
+            </p>
+            <p style={styles.paragraph}>
+              If you are under 18 years of age, reside in California, and have a registered account with the Services or Apps, you have the right to request removal of unwanted data that you publicly post on the Services or Apps. To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Services or Apps, but please be aware that the data may not be completely or comprehensively removed from our systems.
+            </p>
+          </article>
+        </section>
+
+        <section style={styles.section} id='update'>
+          <h3 style={styles.title}>13. DO WE MAKE UPDATES TO THIS POLICY?</h3>
+          <article style={styles.article}>
+            <p style={styles.paragraph}>
+              <em><strong>In short:</strong> Yes, we will update this policy as necessary to stay compliant with relevant laws.</em>
+            </p>
+            <p style={styles.paragraph}>
+              We may update this privacy policy from time to time. The updated version will be indicated by an updated “Revised” date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy policy, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy policy frequently to be informed of how we are protecting your information.
+            </p>
+          </article>
+        </section>
+
+        <section style={styles.section} id='contact'>
+          <h3 style={styles.title}>14. HOW CAN YOU CONTACT US ABOUT THIS POLICY?</h3>
+          <article style={styles.article}>
+            <p style={styles.paragraph}>If you have questions or comments about this policy, you may email us at info@projectmuse.co or by post to:</p>
+            <p style={styles.paragraph}>
+              Project:Muse <br />
+            -----------<br />
+            -----------<br />
+            United States
+            </p>
+          </article>
+        </section>
 
       </section>
     </section>
