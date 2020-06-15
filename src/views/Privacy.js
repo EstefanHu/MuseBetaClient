df8ec4085985e52565d1911046b704f4a97c1425
Estefan Hu <estefanhu074@gmail.com>
2020-06-14 21:58:09 -0700

    added privacy section 1

--- src/views/Privacy.js
+++ src/views/Privacy.js
@@ -4,25 +4,110 @@ export const Privacy = () => {
   return (
     <section style={styles.container}>
       <section style={styles.privacy}>
-        <h1>PRIVACY POLICY</h1>
-        <h2>Last updated June 14, 2020</h2>
-        <article>
-          <p>
-            Thank you for choosing to be part of our community at Project:Muse,
-          doing business as :Muse (“<strong>:Muse</strong>, "<strong>we</strong>”, “<strong>us</strong>”, or “<strong>our</strong>”). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about our policy, or our practices with regards to your personal information, please contact us at info@projectmuse.co.
-      </p>
-          <p>
-            When you visit our website projectmuse.co, mobile application, and use our services, you trust us with your personal information. We take your privacy very seriously. In this privacy policy, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy policy that you do not agree with, please discontinue use of our Sites or Apps and our services.
-        </p>
-          <p>
-            This privacy policy applies to all information collected through our website (such as projectmuse.co), mobile application, ("<strong>Apps</strong>"), and/or any related services, sales, marketing or events (we refer to them collectively in this privacy policy as the "<strong>Services</strong>").
-        </p>
-          <p>
-            <strong>
-              Please read this privacy policy carefully as it will help you make informed decisions about sharing your personal information with us.
-          </strong>
+        <h1 style={styles.header}>PRIVACY POLICY</h1>
+        <h2 style={styles.date}>Last updated June 14, 2020</h2>
+        <section style={styles.section}>
+          <article style={styles.section}>
+            <p style={styles.paragraph}>
+              Thank you for choosing to be part of our community at Project:Muse,
+            doing business as :Muse (“<strong>:Muse</strong>, "<strong>we</strong>”, “<strong>us</strong>”, or “<strong>our</strong>”). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about our policy, or our practices with regards to your personal information, please contact us at info@projectmuse.co.
           </p>
-        </article>
+            <p style={styles.paragraph}>
+              When you visit our website projectmuse.co, mobile application, and use our services, you trust us with your personal information. We take your privacy very seriously. In this privacy policy, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy policy that you do not agree with, please discontinue use of our Sites or Apps and our services.
+          </p>
+            <p style={styles.paragraph}>
+              This privacy policy applies to all information collected through our website (such as projectmuse.co), mobile application, ("<strong>Apps</strong>"), and/or any related services, sales, marketing or events (we refer to them collectively in this privacy policy as the "<strong>Services</strong>").
+          </p>
+            <p style={styles.paragraph}>
+              <strong>
+                Please read this privacy policy carefully as it will help you make informed decisions about sharing your personal information with us.
+            </strong>
+            </p>
+          </article>
+        </section>
+
+        <section>
+          <article style={styles.section}>
+            <h3 style={styles.title}>TABLE OF CONTENTS</h3>
+            <ul style={styles.contentList}>
+              <a href='#whatInfo'><li style={styles.listItem}>1. WHAT INFORMATION DO WE COLLECT?</li></a>
+              <a href='#useInfo'><li style={styles.listItem}>2. HOW DO WE USE YOUR INFORMATION?</li></a>
+              <a href='#willInfo'><li style={styles.listItem}>3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</li></a>
+              <a href='#cookie'><li style={styles.listItem}>4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</li></a>
+              <a href='#google'><li style={styles.listItem}>5. DO WE USE GOOGLE MAPS?</li></a>
+              <a href='#social'><li style={styles.listItem}>6. HOW DO WE HANDLE YOUR SOCIAL LOGINS?</li></a>
+              <a href='#longevity'><li style={styles.listItem}>7. HOW LONG DO WE KEEP YOUR INFORMATION?</li></a>
+              <a href='#safeInfo'><li style={styles.listItem}>8. HOW DO WE KEEP YOUR INFORMATION SAFE?</li></a>
+              <a href='#minorInfo'><li style={styles.listItem}>9. DO WE COLLECT INFORMATION FROM MINORS?</li></a>
+              <a href='#rights'><li style={styles.listItem}>10. WHAT ARE YOUR PRIVACY RIGHTS?</li></a>
+              <a href='#track'><li style={styles.listItem}>11. CONTROLS FOR DO-NOT-TRACK FEATURES</li></a>
+              <a href='#cali'><li style={styles.listItem}>12. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</li></a>
+              <a href='#update'><li style={styles.listItem}>13. DO WE MAKE UPDATES TO THIS POLICY?</li></a>
+              <a href='#contact'><li style={styles.listItem}>14. HOW CAN YOU CONTACT US ABOUT THIS POLICY?</li></a>
+            </ul>
+          </article>
+        </section>
+
+        <section style={styles.section} id='whatInfo'>
+          <h3 style={styles.title}>1. WHAT INFORMATION DO WE COLLECT?</h3>
+          <article style={styles.article}>
+            <h4 style={styles.articleTitle}>Personal information you disclose to us</h4>
+            <p style={styles.paragraph}>
+              <em><strong>In Short: </strong>We collect personal information that you provide to us.</em>
+            </p>
+            <p style={styles.paragraph}>
+              We collect personal information that you voluntarily provide to us when registering at the Services or Apps, expressing an interest in obtaining information about us or our products and services, when participating in activities on the Services or Apps (such as posting messages in our online forums or entering competitions, contests or giveaways) or otherwise contacting us.
+            </p>
+            <p style={styles.paragraph}>
+              The personal information that we collect depends on the context of your interactions with us and the Services or Apps, the choices you make and the products and features you use. The personal information we collect can include the following:
+            </p>
+            <p style={styles.paragraph}>
+              <strong>Publicly Available Personal Information.</strong> We collect first name, maiden name, last name, and nickname; email addresses; phone numbers; social media; and other similar data.
+            </p>
+            <p style={styles.paragraph}>
+              <strong>Personal Information Provided by You. </strong> We collect app usage; passwords; and other similar data.
+            </p>
+            <p style={styles.paragraph}>
+              <strong>Social Media Login Data.</strong> We may provide you with the option to register using social media account details, like your Facebook, Twitter or other social media account. If you choose to register in this way, we will collect the Information described in the section called <a href='#social'>"HOW DO WE HANDLE YOUR SOCIAL LOGINS"</a> below.
+            </p>
+            <p style={styles.paragraph}>
+              All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information.
+            </p>
+          </article>
+          <article style={styles.article}>
+            <h4 style={styles.articleTitle}>Information automatically collected</h4>
+            <p style={styles.paragraph}>
+              <em><strong>In Short: </strong> Some information — such as IP address and/or browser and device characteristics — is collected automatically when you visit our Services or Apps.</em>
+            </p>
+            <p style={styles.paragraph}>
+              We automatically collect certain information when you visit, use or navigate the Services or Apps. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our Services or Apps and other technical information. This information is primarily needed to maintain the security and operation of our Services or Apps, and for our internal analytics and reporting purposes.
+            </p>
+            <p style={styles.paragraph}>
+              Like many businesses, we also collect information through cookies and similar technologies.
+            </p>
+            <p style={styles.paragraph}>
+              <strong>Online Identifiers.</strong> We collect devices; applications; tools and protocols, such as IP (Internet Protocol) addresses; cookie identifiers, or others such as the ones used for analytics and marketing; device's geolocation; Radio Frequency Identification (RFID) tags; and other similar data.
+            </p>
+          </article>
+          <article style={styles.article}>
+            <h4 style={styles.articleTitle}>Information collected through our Apps</h4>
+          </article>
+        </section>
+
+        <section id='useInfo'></section>
+        <section id='willInfo'></section>
+        <section id='cookie'></section>
+        <section id='google'></section>
+        <section id='social'></section>
+        <section id='longevity'></section>
+        <section id='safeInfo'></section>
+        <section id='minorInfo'></section>
+        <section id='rights'></section>
+        <section id='track'></section>
+        <section id='cali'></section>
+        <section id='update'></section>
+        <section id='contact'></section>
+
       </section>
     </section>
   )
@@ -35,8 +120,37 @@ const styles = {
   },
   privacy: {
     backgroundColor: 'white',
-    padding: '30px 50px',
+    padding: '70px 50px 50px 50px',
     borderRadius: '5px',
-
+  },
+  header: {
+    fontSize: '2rem',
+    marginBottom: '20px',
+  },
+  date: {
+    fontSize: '1rem',
+    marginBottom: '60px'
+  },
+  title: {
+    fontSize: '1.8rem',
+  },
+  section: {
+    marginBottom: '60px'
+  },
+  article: {
+    marginTop: '30px'
+  },
+  articleTitle: {
+    fontSize: '1.3rem',
+    marginBottom: '10px',
+  },
+  paragraph: {
+    marginBottom: '15px'
+  },
+  contentList: {
+    listStyle: 'none',
+  },
+  listItem: {
+    marginTop: '15px',
   }
 }
