4f807d1e2a35892671dc16d5cc50e373333cb69d
Estefan Hu <estefanhu074@gmail.com>
2020-06-15 00:07:47 -0700

    removed unused return statements

--- src/views/Forgot.js
+++ src/views/Forgot.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 export const Forgot = () => {
   return (
-    <section>
+    <section style={styles.container}>
       <h1>Hello from Forgot</h1>
+      <form>
+      
+      </form>
     </section>
   )
+}
+
+const styles = {
+  container: {
+    background: 'var(--color)',
+    height: '100vh',
+    padding: '250px 0 0 70px',
+  },
+  title: {
+    fontSize: '2rem',
+  },
 }
