b6b5da270b3d7a91d9d40b2cf12febb583f4c4ec
Estefan Hu <estefanhu074@gmail.com>
2020-06-15 21:02:45 -0700

    updated to styled-components;

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import { GENRES } from '../constants/genre.js';
+import styled from 'styled-components';
 
-const styles = {
-  container: {
-    display: 'block',
-    width: '100%',
-    overflow: 'auto',
-    whiteSpace: 'nowrap',
-    border: '8px solid white'
-  },
-  button: {
-    display: 'inline-block',
-    border: '1px solid lightgrey',
-    borderRadius: '15px',
-    background: 'rgb(245, 245, 245)',
-    fontSize: '.8rem',
-    fontWeight: 'bold',
-    padding: '6px 12px',
-    cursor: 'pointer',
-    margin: '0 5px'
+const Span = styled.span`
+  display: block;
+  width: 100%;
+  overflow: auto;
+  white-space: nowrap;
+  border: 8px solid white;
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  border: 1px solid lightgrey;
+  border-radius: 15px;
+  background: rgb(245,245,245);
+  font-size: 0.8rem;
+  font-weight: bold;
+  padding: 6px 12px;
+  cursor: pointer;
+  margin: 0 5px;
+
+  &:hover {
+    background: rgb(237,237,237);
   }
-}
+`;
 
 export const Filter = ({ setGenre }) => (
-  <span style={styles.container} className='noBar'>
-    <button style={styles.button} onClick={() => setGenre('All')}>All</button>
+  <Span className='noBar'>
+    <Button onClick={() => setGenre('All')}>All</Button>
     {GENRES.map(item => (
-      <button key={item} style={styles.button} onClick={() => setGenre(item)}>{item}</button>
+      <Button key={item} onClick={() => setGenre(item)}>{item}</Button>
     ))}
-  </span>
+  </Span>
 )
