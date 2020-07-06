e8e7575a042ea602cec05a8df480b16a03d4044b
Estefan Hu <estefanhu074@gmail.com>
2020-07-06 15:50:33 -0700

    updated everything

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from 'react-router-dom';
 import decode from 'jwt-decode';
 
@@ -16,14 +15,17 @@ import { FourOhFour } from './views/FourOhFour';
 
 import './App.css';
 
-const AuthRoute = ({ component: Component, ...rest }) => {
-  const { state: { token }, logout } = useContext(AuthContext);
+export const App = () => {
+  const { state: { token }, tryLocalLogin, logout } = useContext(AuthContext);
+  const { approximateLocation } = useContext(LocationContext);
+  const [isLoading, setIsLoading] = useState(true);
   const [isValid, setIsValid] = useState(true);
 
   useEffect(() => {
     try {
       const expDate = decode(token);
-      if (expDate < new Date().getTime() / 1000)
+      console.log(expDate.exp < new Date().getTime() / 1000);
+      if (expDate.exp < new Date().getTime() / 1000)
         setIsValid(false);
     } catch (error) {
       setIsValid(false);
@@ -32,22 +34,6 @@ const AuthRoute = ({ component: Component, ...rest }) => {
     if (!isValid) logout();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
-
-  return (
-    <Route {...rest} render={props => (
-      isValid ? (
-        <Component {...props} />
-      ) : (
-          <Redirect to={{ pathname: '/' }} />
-        )
-    )} />
-  );
-};
-
-export const App = () => {
-  const { state: { token }, tryLocalLogin } = useContext(AuthContext);
-  const { approximateLocation } = useContext(LocationContext);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -65,7 +51,7 @@ export const App = () => {
       <Switch>
         {
           token ?
-            <AuthRoute exact path='/(|new|profile|settings)' component={Primary} />
+            <Route exact path='/(|new|profile|settings)' component={Primary} />
             : <Route exact path='/(|terms|forgot|privacy)' component={Landing} />
         }
         <Route component={FourOhFour} />
